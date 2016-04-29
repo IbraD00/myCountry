0c16115b1ce8f7de1c639525388354e181b7e914
Ibra <ibrahima.dansokho@outlook.com>
2016-04-29 10:27:01 +0200

    fix list

--- app/src/main/java/com/tic_java/ibra_adam/mycountries/MainActivity.java
+++ app/src/main/java/com/tic_java/ibra_adam/mycountries/MainActivity.java
@@ -17,27 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         try {
-           // if(networkAvailable()) {
+            if(networkAvailable()) {
                 response = country.run();
-            //}
-
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         if (response != null) {
             String[] countryArray = new String[response.length];
+            System.out.println(response.length);
             for (int i = 0; i < response.length; i++) {
                 countryArray[i] = response[i].name;
+                System.out.println(response[i].name);
             }
+            System.out.println(response);
             ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_main,R.id.textView, countryArray);
-
             ListView listView = (ListView) findViewById(R.id.listView);
+
             if (listView != null) {
                 listView.setAdapter(adapter);
+                System.out.println("YES");
             }
+        }else {
+            System.out.println("BAD");
         }
     }
 
